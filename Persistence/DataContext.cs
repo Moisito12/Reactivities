9d3f9026726e7d4ebbda33d93aaa5691cdfe1293
moismm1996 <moismm1996@Outlook.com>
2019-12-17 12:14:12 -0600

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+/*
+ Realizar la instalación desde la paquetería nuget de Microsoft.EntityFrameworkCore ysqlite
+ para el Dbcontext
+*/
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        //Creamos el contexto para los datos
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        //Añadimos las propiedades de los valores para ver y modificar
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id=1, Name="Value 101"},
+                new Value {Id=2, Name="Value 102"},
+                new Value {Id=3, Name="Value 103"}
+            );
+        }
+    }
+}
