973498025c9c4de2ce56afc409047569ee65eaa2
moismm1996 <moismm1996@Outlook.com>
2020-01-03 15:51:47 -0600

    End of the section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
+import { Link } from 'react-router-dom';
 
 // Exportamos las propiedades de las actividades
 // y llamamos en la exportación de las constantes
 
 export const ActivityList: React.FC = () => {
     const activityStore = useContext(ActivityStore);
-    const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
+    const {activitiesByDate, deleteActivity, submitting, target} = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
@@ -23,7 +24,7 @@ export const ActivityList: React.FC = () => {
                             </Item.Description>
                             <Item.Extra>
                                 <Button 
-                                    onClick={() => selectActivity(activity.id)} 
+                                    as={Link} to ={`/activities/${activity.id}`}
                                     floated='right' 
                                     content='View' 
                                     color='blue' 
